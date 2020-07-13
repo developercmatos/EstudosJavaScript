77c1c282af260c4df4ae3bdace107f74aa1dab1d
Rafael Matos <developercmatos@gmail.com>
2020-07-12 23:21:59 -0300

    Funções em JS

---
+++ js/main.js
@@ -0,0 +1,84 @@
+
+//Funções
+function soma(n1,n2){
+    return n1 + n2;
+}
+
+function setReplace(frase,nome,novo_nome){
+    return frase.replace(nome,novo_nome);
+}
+
+alert(soma(5,10));
+alert(setReplace("Vai Japão","Japão","Brasil"));
+
+/*
+//Condicionais,laços de repetição e data
+
+var d = new Date();
+alert(d.getDate());
+alert(d.getMonth());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+/*
+var count;
+
+for(count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+
+while(count <= 5){
+    console.log(count);
+    count = count + 1
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade?")
+
+if(idade >= 18){
+alert("Maior de idade")
+}else{
+    alert("Menor de idade")
+}
+*/
+
+/*
+//Dicionario
+var frutas = [{ nome: "maça", cor:"vermelha" },{ nome: "uva", cor:"roxa" }]
+console.log(frutas);
+console.log(frutas[1].nome);
+console.log(frutas[1].cor);
+*/
+/*
+var fruta = { nome: "maça", cor:"vermelha" }
+console.log(fruta.nome);
+console.log(fruta.cor);
+*/
+/*
+var nome = "Rafael Matos"
+var idade1= 35;
+var idade2 = 25;
+var frase = "O Brasil é o meu país"
+//alert(nome + " tem " + idade + " anos");
+alert(idade1 + idade2);
+console.log(nome);
+console.log(idade1);
+console.log(frase);
+*/
+/*
+var lista = ["Banana","Limao","Pêra","Maçã"];
+//add novo item a lista
+lista.push("Uva");
+//remover ultimo item da lista
+//lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join("-"));
+*/
